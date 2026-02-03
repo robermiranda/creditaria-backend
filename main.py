fcdc549162d674cf6c70932b0313280d989930c5
Roberto Miranda Morales <rob.mirandam@gmail.com>
2026-02-03 11:27:55 -0600

    agergando toml file

--- main.py
+++ main.py
@@ -91,4 +91,4 @@ async def simulate(prestamo: Prestamo, background_tasks: BackgroundTasks):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
